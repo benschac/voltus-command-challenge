16f3f02a98e08caedff810459432877b5f445390
Bejamin Schachter <benschac@gmail.com>
2018-07-08 11:35:22 -0400

    hard stop, starts router and querystring functionality

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import App from '../App';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import rootReducer from "../../reducers";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+const store = createStore(rootReducer);
+
 /**
  * The Root of the Application
  * 
  * @class Root
  */
 function Root(){
-  return <App />
+  return [
+    <Provider store={store}>
+      <Route path="/" component={App}/>
+    </Provider>
+  ]
 }
 
 export default Root;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+
+
+/**
+ * The Root Reducer
+ */
+export default combineReducers({
+
+});
